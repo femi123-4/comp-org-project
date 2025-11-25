94f06699e3888a864cfaa2f9916175f9d6650351
femi123-4 <eliasnifemi1@gmail.com>
2025-11-25 16:01:33 -0500

    Add initial data section with prompts and messages

---
+++ project.asm
@@ -0,0 +1,5 @@
+.data
+    prompt:         .asciiz "Please enter an integer N (N >= 22): "
+    error_msg:      .asciiz "Illegal Number!\n"
+    space:          .asciiz " "
+    newline:        .asciiz "\n"
