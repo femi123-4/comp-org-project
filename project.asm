6896c0bf7f2cc110eb055c3809c786d614229644
femi123-4 <eliasnifemi1@gmail.com>
2025-11-25 16:08:18 -0500

    Refactor code structure by adding main function, input validation loop and prompt message

--- project.asm
+++ project.asm
@@ -3,3 +3,14 @@
     error_msg:      .asciiz "Illegal Number!\n"
     space:          .asciiz " "
     newline:        .asciiz "\n"
+
+.text
+.globl main
+
+main:
+    # Step 1: Input validation loop
+input_loop:
+    # Print prompt message
+    li $v0, 4
+    la $a0, prompt
+    syscall
